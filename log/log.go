ea2095f68cffd639e3c2bb79e0897df54ccee343
Ray Guo <ihahoo@163.com>
2017-10-11 23:07:13 +0800

    log工具

---
+++ log/log.go
@@ -0,0 +1,25 @@
+package log
+
+import (
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
+
+var log = logrus.New()
+
+func init() {
+	log.Formatter = &logrus.JSONFormatter{}
+	os.MkdirAll("data/logs/", 0777)
+	file, err := os.OpenFile("data/logs/app.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
+	if err == nil {
+		log.Out = file
+	} else {
+		log.Info("Failed to log to file, using default stderr")
+	}
+}
+
+// GetLog ...
+func GetLog() *logrus.Logger {
+	return log
+}
